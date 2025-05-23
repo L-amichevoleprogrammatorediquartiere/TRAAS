f6e1aa48b58ba9b223c8fd3e82ffc1cded123b9f
Giuseppe Di Sanza <giuseppedisanza2003@gmail.com>
2025-05-23 21:38:00 +0200

    spazziatura della navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,29 +2,28 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
+                        <li className="nav-item custom-margin">
                             <a className="nav-link active" aria-current="page" href="#">Le mie sedute</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item custom-margin">
                             <a className="nav-link" href="#">Libreria Esercizi</a>
                         </li>
-                        <li className="nav-item dropdown">
+                        <li className="nav-item dropdown custom-margin">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             I Tuoi Medici
                             </a>
-                            <ul className="dropdown-menu">
+                            <ul className="dropdown-menu custom-margin">
                                 <li><a className="dropdown-item" href="#">Leonardo Beccardo</a></li>
                                 <li><a className="dropdown-item" href="#">Antonio Banderas</a></li>
                                 <li><a className="dropdown-item" href="#">Giuseppe Cruciani</a></li>
                             </ul>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item custom-margin">
                             <a className="nav-link" href="#">Profilo </a>
                         </li>
                     </ul>
