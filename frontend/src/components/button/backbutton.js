c55c948a12519638a2aecb71c6ec9af6a0d1358e
Giuseppe Di Sanza <giuseppedisanza2003@gmail.com>
2025-05-23 20:48:23 +0200

    Modifica stili buttons modifica app.css e navbar

--- frontend/src/components/button/backbutton.js
+++ frontend/src/components/button/backbutton.js
@@ -1,6 +1,6 @@
 export default function BackButton({ onClick }) {
     return (
-      <button onClick={onClick} className="btn btn-secondary rounded-circle">
+      <button onClick={onClick} className="btn btn-outline-dark px-4 py-1 rounded-0 custom-grey-button">
         <i className="bi bi-arrow-left"></i>
       </button>
     );
